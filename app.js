1419df4622afb36b3d25380a2383e24ce49cef52
sujay2306 <sujayks007@gmail.com>
2021-10-02 15:52:35 +0530

    call-back chaining

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 
 const geocode = require('./utils/geocode')
 const forecast  = require('./utils/forecast')
+const address = (process.argv[2])
 
-geocode('Bangalore', (error,data)=>{
-  console.log('Error',error)
-  console.log('data',data)
+if(!address){
+  console.log("Please provide the adress")
+}
+else{
+  
+geocode(address, (error,data)=>{
+  if(error){
+    return console.log(error);
+  }
 
+  forecast(data.latitude,data.longitude,(error, forecastData) => {
+    if(error){
+      console.log(error);
+    }
+   console.log(data.location);
+   console.log(forecastData);
+  })
 })
 
-forecast(12.972442,77.580643,(error, data) => {
-  console.log("Error", error)
-  console.log("Data", data)
-})
-
+}
 
