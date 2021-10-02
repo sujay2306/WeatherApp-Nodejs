2eba65ac89cc402764b831f7b1dc7b4ae05ad934
sujay2306 <sujayks007@gmail.com>
2021-10-02 21:37:56 +0530

    refactored with shortand and destructruing the code

--- app.js
+++ app.js
@@ -8,16 +8,16 @@ if(!address){
 }
 else{
   
-geocode(address, (error,data)=>{
+geocode(address, (error,{latitude,longitude,location}={})=>{
   if(error){
     return console.log(error);
   }
 
-  forecast(data.latitude,data.longitude,(error, forecastData) => {
+  forecast(latitude,longitude,(error, forecastData) => {
     if(error){
       console.log(error);
     }
-   console.log(data.location);
+   console.log(location);
    console.log(forecastData);
   })
 })
--- utils/forecast.js
+++ utils/forecast.js
@@ -3,15 +3,15 @@ const request = require("request");
 const forecast = (latitude, longitude, callback) => {
   const url = 'http://api.weatherstack.com/current?access_key=347afd7fddd09b141a26541e0e2e3fb1&query=' + latitude +',' + longitude + '&units=f'
 
-  request({ url: url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, {body}) => {
     if (error) {
       callback("Unable to connect to weather service!", undefined);
     } 
-    else if (response.body.error) {
+    else if (body.error) {
       callback("Unable to find location", undefined);
     } 
     else {
-      callback(undefined, response.body.current.weather_descriptions[0]+". it is currently "+response.body.current.temperature+ " degrees out. It feels like "+response.body.current.feelslike+" degrees out"
+      callback(undefined, body.current.weather_descriptions[0]+". it is currently "+body.current.temperature+ " degrees out. It feels like "+body.current.feelslike+" degrees out"
       )
     }
   })
